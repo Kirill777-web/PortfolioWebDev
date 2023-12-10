0f6b2aedb657c049c795a611761278cbe1e30032
Kirill777-web <ki.lazutin@gmail.com>
2023-12-10 18:53:10 -0400

    added files , structure of the app and readme

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './Nav';
+import Sidebar from './Sidebar';
+import Hero from './Hero';
+import About from './About';
+import Services from './Services';
+import Projects from './Projects';
+import Contact from './Contact';
+import Achievements from './Achievements';
+import Resume from './Resume';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className='app-container'>
+        <Nav />
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Hero />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/achievements' element={<Achievements />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/resume' element={<Resume />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
